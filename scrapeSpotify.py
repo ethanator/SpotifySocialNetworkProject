892fa0f3085e7c2fd673483ad97ac7e2a3c0b795
Ethan Yuxuan Chen <ychen18@nd.edu>
2014-11-07 14:26:22 -0500

    can scrape user name and click on the tabs

--- scrapeSpotify.py
+++ scrapeSpotify.py
@@ -11,14 +11,14 @@
 #  - Eliminate the unsupported command-line flag
 #  - Can switch to the user profile page
 #  - Can get iframes
+#  - Can scrape user name
+#  - Can click on the tabs on the user profile
 # ___________________________________________________
 # Version 0.9                     Posted Nov  5, 2014
 #  - Can log in Spotify
 # ===================================================
 
 from bs4 import BeautifulSoup
-import getpass
-import re
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
@@ -26,41 +26,60 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
+import getpass
+import re
+import sys
 import time
 import unicodedata
 
+# Constants
 SPOTIFY = 'http://play.spotify.com/'
 SPOTIFY_USER = 'http://play.spotify.com/user'
 
-# Login to Spotify Web Player
+# Open a broswer and navigate to the Spotify player
 options = webdriver.ChromeOptions()
-options.add_experimental_option("excludeSwitches", ["ignore-certificate-errors"])
+options.add_experimental_option("excludeSwitches", ["ignore-certificate-errors"]) # Suppress a command-line flag
 driver = webdriver.Chrome(chrome_options=options)					
 driver.get(SPOTIFY)
-try:
-  login = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'has-account')))
-  login.click()
-  print 'Spotify Social Network Project'
-  print '=============================='
-  fb_login = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'fb-login-btn')))
-  fb_login.click()
-  driver.switch_to_window(driver.window_handles[1])
-  print 'Logging in via Facebook ...'
-  input_email = raw_input('Email or Phone: ')
-  input_pass  = getpass.getpass('      Password: ')
-  email_blank = driver.find_element_by_id('email')
-  pass_blank  = driver.find_element_by_id('pass')
-  email_blank.send_keys(input_email)
-  pass_blank.send_keys(input_pass)
-  email_blank.submit()
-except:
-  print 'Error!'
 
+# Click the "Already have an account" link
+login = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'has-account')))
+login.click()
+
+# Type in credentials at the command line to log in Spotiy with Facebook
+print 'Spotify Social Network Project'
+print '=============================='
+fb_login = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'fb-login-btn')))
+fb_login.click()
+driver.switch_to_window(driver.window_handles[1])
+print 'Logging in via Facebook ...'
+email_blank = driver.find_element_by_id('email')
+pass_blank  = driver.find_element_by_id('pass')
+input_email = raw_input('Email or Phone: ')
+input_pass  = getpass.getpass('      Password: ')
+email_blank.send_keys(input_email)
+pass_blank.send_keys(input_pass)
+email_blank.submit()
+
+# Navigate from the browse page to the user page
 print 'Waiting for Spotify to load ...'
 time.sleep(10)
 driver.switch_to_window(driver.window_handles[0])
 driver.get(SPOTIFY_USER)
 print 'Waiting for the user profile to load ...'
 time.sleep(10)
-soup = BeautifulSoup(driver.page_source)
-print soup.findAll('iframe', id=re.compile('^user'))
+
+# Locate the user iframe on the page
+iframes = driver.find_elements_by_xpath("//iframe")
+user_iframe = None
+for iframe in iframes:
+	if 'user-app-spotify' in iframe.get_attribute('id'):
+		user_iframe = iframe
+		break
+driver.switch_to_default_content()
+driver.switch_to.frame(user_iframe)
+
+# Scrape the user name and click on the tabs
+print driver.find_element_by_xpath("//h1[@class='h-title']").text
+recent_artists_tab = driver.find_element_by_xpath("//li[@data-navbar-item-id='recently-played-artists']")
+recent_artists_tab.click()
